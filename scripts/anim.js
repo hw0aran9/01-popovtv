25ab9b5cc18277dcc3fa5cff38bb923364f9603c
kolontar-it <admin@itk.com>
2017-03-07 18:46:25 +0400

    reloaded jquery for guaranteed last version redesign in progress

---
+++ scripts/anim.js
@@ -0,0 +1,60 @@
+   $(document).ready(function() {
+    $('li.top-menu-item').hover(
+     function() {
+      $(this).addClass('animated'); // Добавляем класс
+      $(this).addClass('jello');
+     },
+     function() {
+      $(this).removeClass('jello'); // Убираем класс
+      $(this).removeClass('animated'); // Убираем класс
+
+     }
+    )})
+
+ $(document).ready(function() {
+    $('li.top-menu-item-branch').hover(
+     function() {
+      $(this).addClass('animated'); // Добавляем класс
+      $(this).addClass('jello');
+     },
+     function() {
+      $(this).removeClass('jello'); // Убираем класс
+      $(this).removeClass('animated'); // Убираем класс
+     }
+    )})
+
+$(document).ready(function() {
+    $('div.button-plus').hover(
+     function() {
+      $(this).addClass('animated'); // Добавляем класс
+      $(this).addClass('rubberBand');
+     },
+     function() {
+      $(this).removeClass('rubberBand'); // Убираем класс
+      $(this).removeClass('animated'); // Убираем класс
+     }
+    )})
+
+$(document).ready(function() {
+    $('div.button-minus').hover(
+     function() {
+      $(this).addClass('animated'); // Добавляем класс
+      $(this).addClass('rubberBand');
+     },
+     function() {
+      $(this).removeClass('rubberBand'); // Убираем класс
+      $(this).removeClass('animated'); // Убираем класс
+     }
+    )})
+
+$(document).ready(function() {
+    $('img.tits').hover(
+     function() {
+      $(this).addClass('animated'); // Добавляем класс
+      $(this).addClass('jello');
+     },
+     function() {
+      $(this).removeClass('jello'); // Убираем класс
+      $(this).removeClass('animated'); // Убираем класс
+     }
+    )})
